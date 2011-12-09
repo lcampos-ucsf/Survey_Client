b68d8f4a70e8d2237eb0924817dccf7c4c914973
Luis Campos <lcamposguajardo@gmail.com>
2011-12-09 14:39:27 -0800

    fix javascript 404 and execution errors

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
+var j$ = jQuery.noConflict();
+
+j$(document).ready(function(){
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-  $("body").bind("click", function (e) {
-    $('a.menu').parent("li").removeClass("open");
+  j$("body").bind("click", function (e) {
+    j$('a.menu').parent("li").removeClass("open");
   });
 
-  $("a.menu").click(function (e) {
-    var $li = $(this).parent("li").toggleClass('open');
+  j$("a.menu").click(function (e) {
+    var j$li = j$(this).parent("li").toggleClass('open');
     return false;
   });
 
