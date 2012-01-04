46a2107a1d5e47bb679ad3148ea3d10d8d82b405
Luis Campos <lcamposguajardo@gmail.com>
2012-01-04 15:57:48 -0800

    change on my surveys, now show new and in progress results

--- app/widgets/invites_widget.rb
+++ app/widgets/invites_widget.rb
@@ -1,8 +1,7 @@
 class InvitesWidget < Apotomo::Widget
 
   def display
-  	#invite = session[:client].materialize("Invitation__c") 
-  	@invites = session[:client].query("select Id, Name, User__c, Status__c, Start_Date__c, End_Date__c, Survey_Name__c, Progress_Save__c from Invitation__c where User__c = '#{ENV['sf_user']}' and Status__c = 'In Progress' order by LastModifiedDate desc ")
+  	@invites = session[:client].query("select Id, Name, User__c, Status__c, Start_Date__c, End_Date__c, Survey_Name__c, Progress_Save__c from Invitation__c where User__c = '#{ENV['sf_user']}' and (Status__c = 'New' or Status__c = 'In Progress') order by LastModifiedDate desc ")
 
     render
   end
