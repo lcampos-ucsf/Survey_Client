f27e1267bd2954832e2e77d273c4b7ce65400de2
Luis Campos <lcamposguajardo@gmail.com>
2012-01-06 11:58:22 -0800

    ui changes on my survey

--- app/widgets/invites_widget.rb
+++ app/widgets/invites_widget.rb
@@ -1,7 +1,7 @@
 class InvitesWidget < Apotomo::Widget
 
   def display
-  	@invites = session[:client].query("select Id, Name, User__c, Status__c, Start_Date__c, End_Date__c, Survey_Name__c, Survey__c, Survey__r.Description__c, Progress_Save__c from Invitation__c where User__c = '#{ENV['sf_user']}' and (Status__c = 'New' or Status__c = 'In Progress') order by Start_Date__c, LastModifiedDate desc ")
+  	@invites = session[:client].query("select Id, Name, User__c, Status__c, Start_Date__c, End_Date__c, Survey_Name__c, Survey__c, Survey__r.Description__c, Progress_Save__c, Surveyee__c, Surveyee__r.Name, LastModifiedDate from Invitation__c where User__c = '#{ENV['sf_user']}' and (Status__c = 'New' or Status__c = 'In Progress') order by Start_Date__c, LastModifiedDate desc ")
     render
   end
 end
