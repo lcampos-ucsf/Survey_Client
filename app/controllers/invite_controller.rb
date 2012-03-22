2825aa87e2c8dbe002f415a0960d433234916487
Luis Campos <lcamposguajardo@gmail.com>
2012-03-21 20:02:54 -0700

    removed invite index widget

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -13,6 +13,13 @@ class InviteController < ApplicationController
 	end
 	
   def index
+    @invites_query = session[:client].query("select Id, Name, User__c, Status__c, Start_Date__c, End_Date__c, Survey_Name__c, Survey__c, Progress_Save__c, Survey_Subject__c, Survey_Subject__r.Name, LastModifiedDate, Invitation_Subject__c, Is_Preview__c from Invitation__c where User__c = '#{session[:user_id]}' and (Status__c = 'New' or Status__c = 'In Progress') and Is_Preview__c = false order by Status__c, Survey_Name__c, LastModifiedDate desc ")
+
+    #security enhancement
+    @pageno = (params[:page]!= nil) ? ( params[:page].match(/^[0-9]*$/) == nil ? 1 : Sanitize.clean(params[:page]).to_i ) : 1
+    @paginate = 10
+    @invites = Kaminari.paginate_array(@invites_query).page(@pageno).per(@paginate) # Paginates the array
+ 
   end
 
   def new
